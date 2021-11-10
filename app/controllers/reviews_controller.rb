c2729a970e8c992c845defcd5fdcf91a400b1e83
Ahmed BenElHajHamza <benelhajhamza.ahmed@gmail.com>
2021-11-10 16:54:51 +0100

    Yelp MVP done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,16 +7,24 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    if review.save
+    @review.restaurant = @restaurant
+    if @review.save
       redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+
   private
 
   def review_params
-    params.require(:reviews).permit(:rating, :content)
+    params.require(:review).permit(:rating, :content)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
-  validates :name, presence: true
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  # validates :address, presence: true
+  # validates :phone_nubmber, presence: true
+  # validates :name, presence: true
+
 end
