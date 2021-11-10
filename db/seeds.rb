cc78964aecff085a177cf2d9e5d190485f9fe77f
Ahmed BenElHajHamza <benelhajhamza.ahmed@gmail.com>
2021-11-10 12:12:08 +0100

    db seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ puts "Cleaning database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 5 }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 4 }
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 5 }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 4 }
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 5 }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 4 }
+dishoom = { name: "Dishoom", address: "7,Rue corneille Floirac", phone_number: 0534543322, category: "italian" }
+pizza_east =  { name: "Pizza East", address: "9,Rue lormont Bordeaux", phone_number: 0534123456, category: "italian" }
+toto = { name: "Toto", address: "10,Rue cenon Bordeaux", phone_number: 0534542132, category: "japanese" }
+jacki =  { name: "Jacki", address: "1,Rue meriadek Bordeaux", phone_number: 0534540000, category: "japanese"}
+ahmed = { name: "Ahmed", address: "5,Rue alex Bordeaux", phone_number: 0560042132, category: "french"}
+riri =  { name: "Riri", address: "6,Rue rim Bordeaux", phone_number: 0554542132, category: "belgian"}
